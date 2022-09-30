366a87bf78df6d62b92e7ed03001ebfa32b24280
Youssef_Mahmoud <100860879+Youssef-Mhmoud@users.noreply.github.com>
2022-09-30 22:15:38 +0200

    Fix SideBar Showing

--- src/App.js
+++ src/App.js
@@ -53,7 +53,7 @@ const App = () => {
             </div>
           )}
           <div
-            className={`dark:bg-main-bg bg-main-bg min-h-screen md:ml-72 w-full 
+            className={`dark:bg-main-bg bg-main-bg min-h-screen  w-full 
           ${activeMenu ? "md:ml-72" : "flex-1"}`}
           >
             <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -28,7 +28,7 @@ const Sidebar = () => {
             <Link
               to="/"
               onClick={handleCloseSideBar}
-              className="items=center gap-3 ml-3 mt-4 flex text-xl 
+              className="items-center gap-3 ml-3 mt-4 flex text-xl 
               font-extrabold tracking-tight dark:text-white text-slate-900"
             >
               <MdOutlineDashboardCustomize /> <span>DashBoard</span>
