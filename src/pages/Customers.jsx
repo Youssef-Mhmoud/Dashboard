d87041f53e711adaf1a18ce3e88184086b32c523
Youssef_Mahmoud <100860879+Youssef-Mhmoud@users.noreply.github.com>
2022-10-03 21:03:25 +0200

    Crud opretion in Customer Page

--- src/pages/Customers.jsx
+++ src/pages/Customers.jsx
@@ -1,11 +1,41 @@
-import React from 'react'
+import React from "react";
+import {
+  GridComponent,
+  ColumnDirective,
+  ColumnsDirective,
+  Page,
+  Selection,
+  Inject,
+  Edit,
+  Toolbar,
+  Sort,
+  Filter,
+} from "@syncfusion/ej2-react-grids/src";
+import { customersData, customersGrid } from "../data/dummy";
+import { Header } from "../components";
 
 const Customers = () => {
   return (
-    <div>
-      
+    <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="Page" title="Customer" />
+      <GridComponent
+        id="gridcomp"
+        dataSource={customersData}
+        allowPaging
+        allowSorting
+        toolbar={['Delete', 'Add', 'Edit', 'Search']}
+        editSettings={{allowDeleting: true, allowEditing: true, allowAdding: true}}
+        width= 'auto'
+      >
+        <ColumnsDirective>
+          {customersGrid.map((item, i) => (
+            <ColumnDirective key={i} {...item} />
+          ))}
+        </ColumnsDirective>
+        <Inject services={[Selection, Sort, Toolbar, Filter, Page, Edit]} />
+      </GridComponent>
     </div>
-  )
-}
+  );
+};
 
-export default Customers
+export default Customers;
