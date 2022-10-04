6c861f7ac9e5ffdbc336f6763149334979329960
Youssef_Mahmoud <100860879+Youssef-Mhmoud@users.noreply.github.com>
2022-10-04 23:05:10 +0200

    Finish The Calendar Page

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { Navbar, Footer, Sidebar, ThemeSettings } from "./components";
 import {
   Ecommerce,
   Orders,
-  Calender,
+  Calendar,
   Employees,
   Stacked,
   Pyramid,
@@ -74,7 +74,7 @@ const App = () => {
                 {/* Apps */}
                 <Route path="/kanban" element={<Kanban />} />
                 <Route path="/editor" element={<Editor />} />
-                <Route path="/calender" element={<Calender />} />
+                <Route path="/calendar" element={<Calendar />} />
                 <Route path="/color-picker" element={<ColorPicker />} />
 
                 {/* Charts */}
