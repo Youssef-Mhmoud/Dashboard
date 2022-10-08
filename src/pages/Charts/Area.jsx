4d9d92c618d8091621e243ad1f25c7c667968ecb
Youssef_Mahmoud <100860879+Youssef-Mhmoud@users.noreply.github.com>
2022-10-08 21:34:32 +0200

    finish area and line-chart page

--- src/pages/Charts/Area.jsx
+++ src/pages/Charts/Area.jsx
@@ -1,11 +1,45 @@
-import React from 'react'
+import React from "react";
+import {
+  ChartComponent,
+  SeriesCollectionDirective,
+  SeriesDirective,
+  Inject,
+  DateTime,
+  Legend,
+  SplineAreaSeries,
+} from "@syncfusion/ej2-react-charts";
+import {
+  areaCustomSeries,
+  areaPrimaryXAxis,
+  areaPrimaryYAxis,
+} from "../../data/dummy";
+import { useStateContext } from "../../contexts/ContextProvider";
+import { Header } from "../../components";
 
 const Area = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const { currentMode } = useStateContext();
 
-export default Area
+  return (
+    <div className="m-4 md:m-10 mt-24 bg-white dark:bg-secondary-dark-bg p-10 rounded-3xl">
+      <Header category="Area" title="Inflation Rate in Percentage" />
+      <ChartComponent
+        id="area-chart"
+        height="420px"
+        primaryXAxis={areaPrimaryXAxis}
+        primaryYAxis={areaPrimaryYAxis}
+        chartArea={{ border: { width: 0 } }}
+        tooltip={{ enable: true }}
+        background={currentMode === "Dark" ? "#33373E" : "#fff"}
+      >
+        <Inject services={[SplineAreaSeries, DateTime, Legend]} />
+        <SeriesCollectionDirective>
+          {areaCustomSeries.map((item, i) => (
+            <SeriesDirective key={i} {...item} />
+          ))}
+        </SeriesCollectionDirective>
+      </ChartComponent>
+    </div>
+  );
+};
+
+export default Area;
--- src/pages/Charts/Line.jsx
+++ src/pages/Charts/Line.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import {Header, LineChart} from '../../components'
 
 const Line = () => {
   return (
-    <div>
-      
+    <div className='m-4 md:m-10 mt-24 bg-white dark:bg-secondary-dark-bg p-10 rounded-3xl'>
+      <Header category="Charts" title="Inflation Rate"/>
+      <div className='w-full'>
+        <LineChart />
+      </div>
     </div>
   )
 }
