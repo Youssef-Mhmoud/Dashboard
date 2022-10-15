5e3fdd64ab3def192e753dce37ade17713f48d00
Youssef_Mahmoud <100860879+Youssef-Mhmoud@users.noreply.github.com>
2022-10-15 08:22:06 +0200

    adding weekly stats

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -1,17 +1,20 @@
 import React from "react";
 import { BsCurrencyDollar } from "react-icons/bs";
 import { GoPrimitiveDot } from "react-icons/go";
-import { Stacked, Button, SparkLine } from "../components";
+import { Stacked, Button, SparkLine, LineChart } from "../components";
 import {
   earningData,
   SparklineAreaData,
   ecomPieChartData,
   dropdownData,
   recentTransactions,
+  weeklyStats,
+  medicalproBranding,
 } from "../data/dummy";
 import { useStateContext } from "../contexts/ContextProvider";
 import { DropDownListComponent } from "@syncfusion/ej2-react-dropdowns";
 import PieChart from "../components/Charts/PieChart";
+import { IoIosMore } from "react-icons/io";
 
 const DropDown = ({ currentMode }) => (
   <div className="w-28 border-1 border-color px-2 py-1 rounded-md">
@@ -229,9 +232,124 @@ const Ecommerce = () => {
         <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg p-6 rounded-2xl w-96 md:w-760">
           <div className="flex justify-between items-center gap-2 mb-10">
             <p className="text-xl font-semibold">Sales Overview</p>
-            <DropDown currentMode={currentMode}/>
+            <DropDown currentMode={currentMode} />
           </div>
-            <div className="md:w-full overflow-auto"></div>
+          <div className="md:w-full overflow-auto">
+            <LineChart />
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-wrap justify-center">
+        <div className="md:w-400 bg-white dark:text-gray-200 dark:bg-secondary-dark-bg rounded-2xl p-6 m-3">
+          <div className="flex justify-between">
+            <p className="text-xl font-semibold">Weekly Stats</p>
+            <button
+              type="button"
+              className="text-xl font-semibold text-gray-500"
+            >
+              <IoIosMore />
+            </button>
+          </div>
+
+          <div className="mt-10">
+            {weeklyStats.map((item) => (
+              <div
+                key={item.title}
+                className="flex justify-between mt-4 w-full"
+              >
+                <div className="flex gap-4">
+                  <button
+                    type="button"
+                    style={{ background: item.iconBg }}
+                    className="text-2xl hover:drop-shadow-xl duration-300 rounded-full p-3"
+                  >
+                    {item.icon}
+                  </button>
+                  <div>
+                    <p className="text-md font-semibold">{item.title}</p>
+                    <p className="text-sm text-gray-400">{item.desc}</p>
+                  </div>
+                </div>
+                <p className={`text-${item.pcColor}`}>{item.amount}</p>
+              </div>
+            ))}
+            <div className="mt-4">
+              <SparkLine
+                currentColor={currentColor}
+                id="area-sparkLine"
+                height="160px"
+                type="Area"
+                data={SparklineAreaData}
+                width="320"
+                color="rgb(242, 252, 253)"
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className="w-400 bg-white dark:text-gray-200 dark:bg-secondary-dark-bg rounded-2xl p-6 m-3">
+          <div className="flex justify-between">
+            <p className="text-xl font-semibold">MedicalPro Branding</p>
+            <button
+              type="button"
+              className="text-xl font-semibold text-gray-400"
+            >
+              <IoIosMore />
+            </button>
+          </div>
+          <p className="text-xs cursor-pointer hover:drop-shadow-xl duration-300 font-semibold rounded-lg w-24 bg-orange-400 py-0.5 px-2 text-gray-200 mt-10">
+            {" "}
+            16 APR, 2022
+          </p>
+          <div className="flex gap-4 border-b-1 border-color mt-6">
+            {medicalproBranding.data.map((item) => (
+              <div
+                key={item.title}
+                className="border-r-1 border-color pr-4 pb-2"
+              >
+                <p className="text-xs text-gray-400">{item.title}</p>
+                <p className="text-sm">{item.desc}</p>
+              </div>
+            ))}
+          </div>
+          <div className="border-b-1 border-color pb-4 mt-2">
+            <p className="text-md font-semibold mb-2">Teams</p>
+            <div className="flex gap-4">
+              {medicalproBranding.teams.map((item) => (
+                <p
+                  key={item.name}
+                  style={{ background: item.color }}
+                  className="cursor-pointer hover:drop-shadow-xl duration-300 text-white py-0.5 px-3 rounded-lg text-xs"
+                >
+                  {item.name}
+                </p>
+              ))}
+            </div>
+          </div>
+          <div className="mt-2">
+            <p className="text-md font-semibold mb-2">Leaders</p>
+            <div className="flex gap-4">
+              {medicalproBranding.leaders.map((item, i) => (
+                <img
+                  key={i}
+                  className="rounded-full w-8 h-8"
+                  src={item.image}
+                  alt=""
+                />
+              ))}
+            </div>
+          </div>
+          <div className="flex justify-between items-center mt-5 border-t-1 border-color">
+            <div className="mt-3">
+              <Button
+                color="white"
+                bgColor={currentColor}
+                text="Add"
+                borderRadius="10px"
+              />
+            </div>
+            <p className=""></p>
+          </div>
         </div>
       </div>
     </div>
